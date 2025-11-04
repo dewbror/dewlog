d6af6e1933193af74b4dfbf0313edc415a0a7c65
dewb <dewb@localhost.localdomain>
2025-11-04 16:32:47 +0100

    Fix tests script

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 tests_exe="c_log_tests"
+file="c_log"
 
 dir=""
 
@@ -28,7 +29,7 @@ echo ""
 if [ "${dir}" = "./build_debwithcov" ]; then
     echo "Generating coverage info"
     lcov --capture --directory "${dir}/tests/CMakeFiles" --output-file "${dir}/coverage.info" > /dev/null 2>&1 || exit 1
-    lcov --extract "${dir}/coverage.info" '*/c_log.h' --output-file "${dir}/coverage.c_log.info" 2>/dev/null || exit 1
-    genhtml "${dir}/coverage.c_log.info" --output-directory "${dir}/coverage_html" > /dev/null 2>/dev/null || exit 1
+    lcov --extract "${dir}/coverage.info" "*/${file}.h" --output-file "${dir}/coverage.${file}.info" 2>/dev/null || exit 1
+    genhtml "${dir}/coverage.${file}.info" --output-directory "${dir}/coverage_html" > /dev/null 2>/dev/null || exit 1
     rm "${dir}/coverage.info" || exit 1
 fi
